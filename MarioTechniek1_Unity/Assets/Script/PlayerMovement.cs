bb460cd11cfa218ca0577410aa7887ff56f86c74
mangopudding12 <josemcrum@hotmail.nl>
2015-03-09 20:12:44 +0100

    MarioTechniek_Unity
    Spannend I am trying to learn Unity. :hamster:  It is also for my isa.

---
+++ MarioTechniek1_Unity/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private float maxSpeed = 5f; 
+
+	private Vector3 input; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3 (Input.GetAxisRaw ("Horizontal"),0, Input.GetAxisRaw("Vertical")); 
+
+		if (rigidbody.velocity.magnitude < maxSpeed) 
+		{
+			rigidbody.AddForce (input*moveSpeed);
+		}
+
+
+
+
+	}
+}
