54e71a1dd94b08f65b27e1e70413d94f7c861f8b
mangopudding12 <josemcrum@hotmail.nl>
2015-03-14 17:42:00 +0100

    MarioTechniek_Unity
    Exercises and Trying stuff

---
+++ Experiment_Unity/Assets/scripts/Jump.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	public float CaviaJump; 
+	public Vector3 CaviaJumpRichting;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// Jumping code 
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			print ("awawawawa");
+			rigidbody.AddForce (Vector3.up * CaviaJump); 
+		}
+
+
+
+	}
+}
