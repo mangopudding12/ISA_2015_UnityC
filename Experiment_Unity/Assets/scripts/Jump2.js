56048d562f9e07717251928d7affe3b7f2b148a4
mangopudding12 <josemcrum@hotmail.nl>
2015-03-15 18:06:52 +0100

    MarioTechniek_Unity
    OOOOOOO I hate unity so much now at this moment. I understand nothing of
    it. I hope I will understand in the future

---
+++ Experiment_Unity/Assets/scripts/Jump2.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var jumpHeight = 8;
+
+private var isFalling = false;
+
+function Update ()
+{
+
+	if (Input.GetKeyDown(KeyCode.Space) && isFalling == false)
+	{
+		rigidbody.velocity.y = jumpHeight;
+	}
+	
+	// This makes the ball jump one time not two times. 
+	isFalling = true;
+}
+
+function OnCollisionStay ()
+{
+	isFalling = false;
+}
