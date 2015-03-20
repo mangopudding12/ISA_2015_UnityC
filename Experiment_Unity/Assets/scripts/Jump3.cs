579a781d0f3b48768151a33fa6045be25d92e081
mangopudding12 <josemcrum@hotmail.nl>
2015-03-20 20:51:27 +0100

    MarioTechniek_Unity
    It is jumping and moving. Not on the way I wanna but It is working. So I
    go to the next thing. :satisfied:

--- Experiment_Unity/Assets/scripts/Jump3.cs
+++ Experiment_Unity/Assets/scripts/Jump3.cs
@@ -4,25 +4,60 @@ using System.Collections;
 public class Jump3 : MonoBehaviour {
 	
 	public float CaviaJump; 
+	public float moveSpeed; 
+	public float maxSpeed;
+
+
+	private float maxSpeedAir; 
+	private float moveSpeedAir;
 	private bool Ground; 
-	//public Vector3 CaviaJumpRichting;
+	private Vector3 input; 
+	private Vector3 jump;
 	
 	// Use this for initialization
 	void Start () {
 		Ground = false; 
+
+		maxSpeedAir = 15f;
+		moveSpeedAir = 5f;
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
-		
+
 		// Jumping code 
-		if (Input.GetKeyDown (KeyCode.Space) && Ground == false) {
-			rigidbody.AddForce (Vector3.up * CaviaJump); 
-			Ground = true; 
-		} 
+		if (Input.GetKeyDown (KeyCode.Space) && Ground == false) 
+		{
+			jump = new Vector3 (0,1,0);
+			rigidbody.AddForce ( jump * CaviaJump); 
+			Ground = true;
+		}
+
+
+
+		if (rigidbody.position.y > 2) {
+			// Moving in Air slower. 
+			// This left, right, backwards, forwards movement.
+			input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")); 
+			if (rigidbody.velocity.magnitude < maxSpeedAir) {
+				rigidbody.AddForce (input * moveSpeedAir);
+			}
+		} else {
+			// Moving faster on the ground.
+			// This left, right, backwards, forwards movement.
+			input = new Vector3 (Input.GetAxisRaw ("Horizontal"),0, Input.GetAxisRaw("Vertical")); 
+			if ((rigidbody.velocity.magnitude < maxSpeed)) 
+			{
+				rigidbody.AddForce (input*moveSpeed);
+			} 
+		}
+
+
 
 	}
 
+	// Can only jump when it is on the ground.
 	void OnCollisionEnter ()
 	{
 		Ground = false;
---
+++ Experiment_Unity/Assets/scripts/MoveCircel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCircel : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float turnSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+			if(Input.GetKey(KeyCode.UpArrow)){
+				transform.Translate(Vector3.forward * Time.deltaTime);
+				}
+		
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(Vector3.forward * -moveSpeed * Time.deltaTime);
+		}
+
+	}
+}
