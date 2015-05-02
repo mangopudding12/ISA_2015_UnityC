6122844ef12c61055ef62cb8ac1440cc9931f4d7
mangopudding12 <josemcrum@hotmail.nl>
2015-05-02 21:21:24 +0200

    MarioTechniek2_Unity :octocat:
    I am beginning again with unity. It is still so weird for me
    :open_mouth:

---
+++ MarioTechniek2_Unity/Assets/Code/FollowPath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic; // Liberia voor de IEnumerator. 
+using UnityEngine;
+using System.Collections;
+
+public class FollowPath : MonoBehaviour {
+
+	// Hier kan je een lijstje van namen zetten. 
+	public enum FollowType 
+	{
+		MoveTowards, 
+		Lerp
+	}
+
+	// FollowType.MoveTowards = een default setting. 
+	public FollowType Type = FollowType.MoveTowards;
+	public PathDefinition Path; 
+	public float Speed = 1; 
+	public float MaxDistanceToGoal =.1f; // The max afstand the object moet hebben voordat die naar volgende piont toe gaat. 
+
+	// IEumerator weet the goeie volgorde van onze punten. 
+	private IEnumerator<Transform>_currentPoint; 
+
+
+	public void Start() 
+	{
+	}
+
+}
---
+++ MarioTechniek2_Unity/Assets/Code/PathCode.cs
@@ -0,0 +1,63 @@
+﻿using System; // Liberia for NotImplementedException. 
+using System.Collections.Generic; // Is liberia for IEnumerator. 
+using UnityEngine;
+using System.Collections;
+
+public class PathCode : MonoBehaviour 
+{
+	public Transform[] Points; 
+
+	// iets wat heel goed eerst naar het ene kan kijken en dan naar de volgende, soort volgorde denk ik.
+	// Wordt vaak gebruikt voor routines 
+	public IEnumerator <Transform> GetPathEnumerator()
+	{
+		if (Points == null || Points.Length < 1) 
+		{
+			yield break; // stop de funtie meteen. 
+		}
+
+		var direction = 1;
+		var index = 0; 
+
+		// Gaat oneindig door. Hij check the pionts zodat onze object er langs blijft bewegen.
+		while (true) 
+		{
+			yield return Points[index];
+
+			if (Points.Length == 1)
+			{
+				continue; 
+			}
+
+			if (index <= 0)
+			{
+				// Objects gaat van links naar rechts 
+				direction = 1; 
+			} else if (index >= Points.Length-1){
+				// Objects gaat van rechts naar links 
+				direction = -1; 
+			}
+
+			index = index + direction;
+		} // end while loop
+	}
+
+	// Alles wat hier in staat kan je ook in scenen zien. 
+	public void OnDrawGizmos() 
+	{
+		// Als er minder dan twee lijnen zijn ga dan weer terug naar de void funcitie. 
+		if (Points == null || Points.Length < 2) 
+		{
+			return;
+		}
+
+		for (int i = 1; i < Points.Length; i++) 
+		{
+			// draw a line between the points that in the array
+			Gizmos.DrawLine(Points[i-1].position,Points[i].position);  
+		}
+
+	}
+}
+
+
