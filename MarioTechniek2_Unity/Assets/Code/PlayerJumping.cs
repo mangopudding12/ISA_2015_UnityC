ae027693ca90557de7f7d0470c2d1b6ac8982790
mangopudding12 <josemcrum@hotmail.nl>
2015-05-09 17:07:14 +0200

    Now you can kill enemy
    Use the D or F button to destroy the enemy's. You can jump with K En
    walk with J en L hihi :cactus:

--- MarioTechniek2_Unity/Assets/Code/PlayerJumping.cs
+++ MarioTechniek2_Unity/Assets/Code/PlayerJumping.cs
@@ -4,23 +4,44 @@ using System.Collections;
 public class PlayerJumping : MonoBehaviour {
 
 	public bool ground = false; 
-	public Transform lineStart,lineEnd; 
+	public bool wall = false; 
+	public Transform groundEnd; 
+	public float JumpForce; 
 
 	// Update is called once per frame
 	void Update () 
 	{
 		lineCasting (); 
+		Jumping ();
 	}
 
 	void lineCasting() 
 	{
-		Debug.DrawLine(lineStart.position,lineEnd.position,Color.green);
+		Debug.DrawLine(this.transform.position,groundEnd.position,Color.green);
 
-
-
-
-		
+		// Als the player the layer ground aanraakt dan ground will be true. 
+		ground = Physics2D.Linecast(this.transform.position,groundEnd.position, 1 << LayerMask.NameToLayer("Ground"));
+		wall = Physics2D.Linecast(this.transform.position,groundEnd.position, 1 << LayerMask.NameToLayer("Wall"));
 	}
 
+	void Jumping() 
+	{
+		if (transform.position.y >= 70) {
+			print ("Wacht rustig tot player weer in scherm is Sorry");
+		} else {
+			print (""); 
+		}
 
+		if (Input.GetKey (KeyCode.K) && ground == true) 
+		{
+				JumpForce = 9000; 
+				rigidbody2D.AddForce(Vector2.up * JumpForce);
+		}
+
+		if (Input.GetKey (KeyCode.K)&& wall == true)
+		{
+			JumpForce = 4000; 
+			rigidbody2D.AddForce(Vector2.up * JumpForce);
+		} 
+	}
 }
--- MarioTechniek2_Unity/Assets/Code/PlayerMovement.cs
+++ MarioTechniek2_Unity/Assets/Code/PlayerMovement.cs
@@ -4,20 +4,64 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speedPlayer; 
+	public bool interact = false; 
+	public Transform lineStart,lineEnd; 
+	RaycastHit2D whatIHit; 
 
 
 	// Update is called once per frame
 	void Update () 
 	{
 		Movement (); 
+		Reycast (); 
+		Hitting ();
 	}
+
+	void Reycast() 
+	{
+		Debug.DrawLine (lineStart.position, lineEnd.position, Color.red);
+
+		if (Physics2D.Linecast (lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer ("Vijand"))) 
+		{
+				// Hier stop je hetgene wat je hebt geraakt in een whatIHit name. 
+			    // Hierdoor kun je het straks bijvoorbeeld vernietigen. 
+				whatIHit = Physics2D.Linecast (lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer ("Vijand"));
+				interact = true; 
+				
+		}
+		else 
+		{ 
+				interact = false; 
+		}
+
+		// Now the player layer Igonere the Collision with de enemy layer. 
+		// The numbers are de numers of the layer. 
+		// 8 is the player and 9 are all the enemy's. 
+		Physics2D.IgnoreLayerCollision (8, 9);
+	}
+
+	void Hitting() 
+	{
+		if (Input.GetKeyDown (KeyCode.D) && interact == true) 
+		{
+			//Destroy gameobject whatIHit, it is not in the world anymore. 
+			Destroy (whatIHit.collider.gameObject); 
+		}
+
+		if (Input.GetKeyDown (KeyCode.F) && interact == true) 
+		{
+			// Set the checkbox out, so you don't see it anymore. 
+			whatIHit.collider.gameObject.SetActive(false);
+		}
+	}
+
+
 
 	void Movement() 
 	{
 		// Time.deltaTime = 
 		// When you multiply with Time.deltaTime you essentially express: 
 		// I want to move this object 10 meters per second instead of 10 meters per frame.
-
 
 
 		// Rechts movement 
