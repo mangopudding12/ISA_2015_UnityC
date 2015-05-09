2d57f476e06fdcfb9783818fc5e4f7eb5e72cb7f
mangopudding12 <josemcrum@hotmail.nl>
2015-05-09 20:32:43 +0200

    AAAAA :smile_cat: Enemy is moving
    I begin to understand unity :fish: :panda_face: It is nice and much
    easier than processing.

---
+++ MarioTechniek2_Unity/Assets/Code/Enemy_Movement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+	public class Enemy_Movement : MonoBehaviour {
+	public float snelheid; 
+	public Transform Cstart,Cend; 
+	public bool TouchSometing; 
+
+	// Deze manier van LineCasting is iets minder mooi. 
+	// want zodra je de lijn van uit je player of enemy wilt laten beginnen. 
+	// Reageert de LineCast op het eigen lichaam van de enemy. 
+	// Dus dan is TouchSomething dus de hele tijd true false true false. 
+	// Maar zodra het uit zijn eigen collider is dan werkt het perfect. 
+	// Belangrijk het reageert dus op welke collider dan ook. 
+	
+	void Update () {
+
+		LineCast (); 
+		Moving (); 
+
+	}
+
+	void Moving () 
+	{
+		// Give the movement to the right 
+
+	}
+
+	void LineCast() 
+	{
+		rigidbody2D.velocity = new Vector2 (snelheid,rigidbody2D.velocity.y);
+
+		// LineCasting kijken of je tegen muur bots. 
+		TouchSometing = Physics2D.Linecast(Cstart.position,Cend.position);
+		print (TouchSometing); 
+
+		if (TouchSometing == true) 
+		{
+			transform.localScale = new Vector2 (transform.localScale.x * -1, transform.localScale.y);
+			snelheid *= -1; 
+		}
+	}
+
+	// Dit is ook een manier om onzichtbare lijnen zichtbaar te maken. 
+	void OnDrawGizmos() 
+	{
+		Gizmos.color = Color.cyan; 
+		Gizmos.DrawLine (Cstart.position, Cend.position); 
+	}
+
+}
